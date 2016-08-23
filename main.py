287773c30df7e6d4201ae67a598ead086071b505
Travis <shaggyhair@gmail.com>
2016-08-23 06:57:34 -0500

    updating first round

--- main.py
+++ main.py
@@ -1,25 +1,78 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+from ceaser import encrypt
+import cgi
 
-class MainHandler(webapp2.RequestHandler):
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>first assignment</title>
+    <style type="text/css">
+        form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+            p.error {
+                color: red;
+            }
+    </style>
+</head>
+<body>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        rotation = """
+        <form action="/answer" method="post">
+            <label for="rotation1">Rotate By:</label>
+                <input name="rotation1" type="number" value="0">
+                <p class="error"></p>
+            <textarea type="text" name="text1"></textarea>
+            <br>
+            <input type="submit">
+        </form>
+        """
+
+        self.response.write(page_header + rotation + page_footer)
+
+class Answer(webapp2.RequestHandler):
+    def post(self):
+        text = self.request.get("text1")
+        rotation = self.request.get("rotation1")
+        answer2 = encrypt(text, int(rotation))
+
+        rotationDone = """
+        <form>
+            <label for="rotation1">Rotate By:</label>
+                <input type="number>""" + rotation + """</input>
+                <p class="error"></p>
+            <textarea type="text">""" + answer2 + """</textarea>
+            <br>
+            <input type="submit"></input>
+        </form>
+        """
+
+
+        self.response.write(page_header + rotationDone + page_footer)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/answer', Answer)
 ], debug=True)
